47357db2104fbd26a53530c1bed81d3d778febef
gingrm02 <89487245+gingrm02@users.noreply.github.com>
2023-10-05 15:30:42 -0400

    Base module class to provide a concrete interface to config generation modules (#1)
    Co-authored-by: Robert Gingras <developer@three-point-five.dev>

---
+++ src/modules/base.py
@@ -0,0 +1,40 @@
+from typing import Self
+from pathlib import Path
+
+class Module:
+    def generate(self: Self) -> str:
+        """Generates the content of the configuration file."""
+
+        throw NotImplementedError
+    #end generate
+    
+    def install_location(self: Self) -> Path:
+        """Returns the location that the config file should be installed to.
+        This path should always be absolute. Relative paths will be assumed
+        to be relative to the root directory.
+        """
+
+        throw NotImplementedError
+    #end install_location
+
+    def install(self: Self, chroot: Path = Path('/')):
+        """Default implementation of the installation procedure. Without chroot
+        this will likely require the application is ran as root.
+        """
+        
+        install_file = Path(chroot, self.install_location())
+        with open(self.install_location(), 'w') as fd:
+            fd.write(self.generate())
+        #end with
+    #end install
+
+    def validate(self: Self) -> bool:
+        """Validates the configuration by attempting to generate the configuration file."""
+
+        try:
+            self.generate()
+            return true
+        except:
+            return false
+    #end validate
+#end class Module
