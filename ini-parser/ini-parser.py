4cbac1887a0f6251c148ac2bfdbe6b43e14585b1
Bobzemob <greeht01@pfw.edu>
2023-10-06 16:47:06 -0400

    Update 1
    - Added helper methods to ini-parser.py

--- ini-parser/ini-parser.py
+++ ini-parser/ini-parser.py
@@ -3,16 +3,43 @@ import pathlib
 
 currentDirectory = pathlib.Path().resolve() # Returns windowsPath obj representing working directory (\genisys\)
 
-def parse(filename = currentDirectory.__str__() + '\\ini-parser\\test.ini'):
+''' Accepts a read config and converts it into a traditional python dictionary of strings object. If "removeComments" is true, comments will be stripped from the final dictionary. '''
+def MakeDictionary(config, removeComments):
+    
+    
+    if(removeComments):
+        config = removeComments(config)
+    
+
+'''  '''
+def removeComments(config):
+    
+    #TODO#
+    # for section in config.sections():
+    #     print((config[section]))
+
+    return config
+
+
+''' Prints out the entire content of a ConfigParser object to the console as key value pairs. '''
+def printConfigFile(config): 
+    for section in config.sections():
+        print(section)
+        print(config.items(section))
+
+
+''' Returns a ConfigParser object containing the contents of the specified .ini file, currently assumes that file is located in C:/ ... /genisys/ini-parser/ '''
+def parse(file):
+    file = currentDirectory.__str__() + '\\ini-parser\\' + file # Non-default
     config = configparser.ConfigParser() # Create parser object
+    config.read(file) # Read contents of the INI file
+    return config
 
-    config.read(filename) # Read contents of the INI file
-
-    print("Filename: " + filename)
-    print(config.sections()) 
 
 def main():
-    parse()
+    config = parse('test.ini')
+    configDictionary = MakeDictionary(config, False)
+    # printConfigFile(config)
 
 if __name__ == '__main__':
     main()
