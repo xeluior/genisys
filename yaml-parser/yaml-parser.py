3ea30047503748b9852e1eadc5889640c50786ee
Bobzemob <89527529+HenrithicusGreenson@users.noreply.github.com>
2023-10-07 15:45:45 -0400

    Added getSection
    Returns a python dictionary that holds the key value pairs of a YAML file.
    
    Currently accepts command line arguments as filename and section heading.

--- yaml-parser/yaml-parser.py
+++ yaml-parser/yaml-parser.py
@@ -1,17 +1,48 @@
-import pathlib
-import yaml
+''' 
+argv[1] = yaml file to parse 
+argv[2] = section in yaml file to grab (If this is absent, return entire file as dictionary)
+'''
+import sys
+import yaml # Look into using ruamel.yaml for using YAML 1.2 specification
 
-yaml = yaml()
-path = pathlib.Path('test.yml')
+''' Returns all of the key value pairs under a specific heading as a Python dictionary '''
+def getSection(fileName, heading):
+    dictionary = {}
+    with open(fileName) as file:
+        data = yaml.safe_load(file)
+
+        if heading in data:
+            sectionData = data[heading]
+
+            for key, value in sectionData.items():
+                dictionary[key] = value
+
+        else:
+            print('Heading \'' + heading + '\' not found.')
+    
+    return dictionary
+
+# def getAllSections(fileName):
+#     dictList = [{}]
+#     with open(fileName) as file:
+#         data = yaml.safe_load(file)
+
+#         for heading in data:
+#             sectionData = data[heading]
+
+#             for key, value in sectionData.items():
 
 
-def parseYaml(fileName):
-    with open('my_file.yaml') as file:
-        l = yaml.load(file)
-        print(l)
+def printDict(dictionary):
+    for key in dictionary:
+        print(f"{key}: {dictionary[key]}")
 
 def main():
-    parseYaml('test.yml')
+    if len(sys.argv) > 2:
+        printDict(getSection(sys.argv[1], sys.argv[2]))
+    else:
+        print('unimplemented')
+
 
 if __name__ == '__main__':
     main()
