c8b9bfccbc587065d12df38ec3dc8c7d9a524533
Bobzemob <greeht01@pfw.edu>
2023-10-09 14:18:14 -0400

    Added getAllHeadings method.
    - Added getAllHeadings, which returns each "section" in the yaml file.
    
    NOTE: Each section must have at least one key value pair in it, otherwise an error is handled that returns a statement saying that the specified section contains no values.

--- yaml-parser/yaml-parser.py
+++ yaml-parser/yaml-parser.py
@@ -1,8 +1,3 @@
-''' 
-argv[1] = yaml file to parse 
-argv[2] = section in yaml file to grab (If this is absent, return entire file as dictionary)
-'''
-import sys
 import yaml # Look into using ruamel.yaml for using YAML 1.2 specification
 
 ''' Returns all of the key value pairs under a specific heading as a Python dictionary '''
@@ -13,36 +8,38 @@ def getSection(fileName, heading):
 
         if heading in data:
             sectionData = data[heading]
+            print('Current Heading: ' + heading)
 
-            for key, value in sectionData.items():
-                dictionary[key] = value
+            try:
+                for key, value in sectionData.items():
+                    dictionary[key] = value
+            except AttributeError:
+                print('Heading', heading, 'contains no values.')
 
         else:
             print('Heading \'' + heading + '\' not found.')
     
     return dictionary
 
-# def getAllSections(fileName):
-#     dictList = [{}]
-#     with open(fileName) as file:
-#         data = yaml.safe_load(file)
+''' Returns a list object containing all section headings in the provided YAML file '''
+def getAllHeadings(fileName):
+    headingList = []
+    with open(fileName) as file:
+        data = yaml.safe_load(file)
 
-#         for heading in data:
-#             sectionData = data[heading]
+        for heading in data:
+            headingList.append(heading)
 
-#             for key, value in sectionData.items():
-
+    return headingList
 
 def printDict(dictionary):
     for key in dictionary:
         print(f"{key}: {dictionary[key]}")
 
 def main():
-    if len(sys.argv) > 2:
-        printDict(getSection(sys.argv[1], sys.argv[2]))
-    else:
-        print('unimplemented')
-
+    print(getAllHeadings('test.yml'))
+    for eachSection in getAllHeadings('test.yml'):
+        printDict(getSection('test.yml', eachSection))
 
 if __name__ == '__main__':
     main()
