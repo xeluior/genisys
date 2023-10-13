8338ead8e37a7100d052f8dcf7e199227e1213da
Bobzemob <greeht01@pfw.edu>
2023-10-13 14:41:59 -0400

    Changed structure to class
    Added Self

--- yaml-parser/yaml-parser.py
+++ yaml-parser/yaml-parser.py
@@ -1,45 +1,55 @@
 import yaml # Look into using ruamel.yaml for using YAML 1.2 specification
+from typing import Self
 
-''' Returns all of the key value pairs under a specific heading as a Python dictionary '''
-def getSection(fileName, heading):
-    dictionary = {}
-    with open(fileName) as file:
-        data = yaml.safe_load(file)
+class YAMLParser:
+    def __init__(self, fileName) -> None:
+        self.fileName = fileName
+    # end __init__
 
-        if heading in data:
-            sectionData = data[heading]
-            print('Current Heading: ' + heading)
+    ''' Returns all of the key value pairs under a specific heading as a Python dictionary '''
+    def getSection(self: Self, heading):
+        dictionary = {}
+        with open(self.fileName) as file:
+            data = yaml.safe_load(file)
 
-            try:
-                for key, value in sectionData.items():
-                    dictionary[key] = value
-            except AttributeError:
-                print('Heading', heading, 'contains no values.')
+            if heading in data:
+                sectionData = data[heading]
+                print('Current Heading: ' + heading)
 
-        else:
-            print('Heading \'' + heading + '\' not found.')
-    
-    return dictionary
+                try:
+                    for key, value in sectionData.items():
+                        dictionary[key] = value
+                except AttributeError:
+                    print('Heading', heading, 'contains no values.')
 
-''' Returns a list object containing all section headings in the provided YAML file '''
-def getAllHeadings(fileName):
-    headingList = []
-    with open(fileName) as file:
-        data = yaml.safe_load(file)
+            else:
+                print('Heading \'' + heading + '\' not found.')
+        
+        return dictionary
+    # end getSection
 
-        for heading in data:
-            headingList.append(heading)
+    ''' Returns a list object containing all section headings in the provided YAML file '''
+    def getAllHeadings(self: Self):
+        headingList = []
+        with open(self.fileName) as file:
+            data = yaml.safe_load(file)
 
-    return headingList
+            for heading in data:
+                headingList.append(heading)
 
-def printDict(dictionary):
-    for key in dictionary:
-        print(f"{key}: {dictionary[key]}")
+        return headingList
+    # end getAllHeadings
+
+    def printDict(self: Self, dictionary):
+        for key in dictionary:
+            print(f"{key}: {dictionary[key]}")
+    # end printDict
 
 def main():
-    print(getAllHeadings('test.yml'))
-    for eachSection in getAllHeadings('test.yml'):
-        printDict(getSection('test.yml', eachSection))
+    parser = YAMLParser('test.yml') 
+    print(parser.getAllHeadings())
+    for eachSection in parser.getAllHeadings():
+        parser.printDict(parser.getSection(eachSection))
 
 if __name__ == '__main__':
     main()
