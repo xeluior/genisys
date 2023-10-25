2709c22806a0bf128d0621491b54cc5a5775a04c
unknown <RGingras@pplastic.com>
2023-10-25 13:41:14 -0400

    added setup_commands function to base Module

--- genisys/modules/base.py
+++ genisys/modules/base.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 from typing_extensions import Self
+from abc import ABCMeta, abstractmethod
 
-class Module:
+class Module(metaclass=ABCMeta):
     """Base class all module should inherit from"""
+    @abstractmethod
     def generate(self: Self) -> str:
         """Generates the content of the configuration file."""
 
         raise NotImplementedError
     #end generate
 
+    @abstractmethod
     def install_location(self: Self) -> Path:
         """Returns the location that the config file should be installed to.
         This path should always be absolute. Relative paths will be assumed
@@ -38,4 +41,11 @@ class Module:
         except:
             return False
     #end validate
+
+    def setup_commands(self: Self) -> list[str] | list[list[str]]:
+        """Returns commands which are should be ran before the module's configuration output is completed.
+        Should return a list such that each item can be passed to the subprocess.run() function.
+        """
+        return []
+    # end setup
 #end class Module
