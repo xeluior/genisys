8d46cd9225531f9720c6032bc14bf77ddc5e2b41
BBergle <bennybergle@icloud.com>
2023-11-03 14:25:03 -0400

    Added comments and setup commands. Added FTP in yml file

--- genisys/modules/FTP.py
+++ genisys/modules/FTP.py
@@ -1,38 +1,42 @@
 # FTP.py
 from pathlib import Path
-from typing_extensions import Self
+from typing_extensions import List, Union
+# Import the Module class from which VsftpdModule will inherit
 from genisys.modules.base import Module
 
-
 class VsftpdModule(Module):
-    def __init__(self, anonymous_enable: bool = True):
-        self.anonymous_enable = anonymous_enable
-
-    def generate(self: Self) -> str:
-        """Generates the content of the vsftpd configuration file."""
+    def __init__(self, config):
+        # Obtain the 'ftp' section from the 'Network' configuration
+        self.config = config.getSection("Network")["ftp"]
+        # Obtain the entire 'Network' section from the configuration
+        self.network = config.getSection("Network")
+        # Retrieve the IP address for the FTP service to bind to
+        self.bind_addr = self.network.get("ip")
+        
+    def generate(self) -> str:
+        # Retrieve the directory path where the FTP service should root its file system
+        directory = self.config["directory"]
+        # Retrieve the FTP service port from the configuration
+        ftpPort = self.config["ftp-port"]
+        # Use the bind address obtained from the configuration
+        bindAddr = self.bind_addr
+        # Assemble the configuration lines for vsftpd.conf
         config_lines = [
-            "listen=NO",
-            f"anonymous_enable={'YES' if self.anonymous_enable else 'NO'}",
-            "local_enable=YES",
-            "write_enable=YES",
-            "dirmessage_enable=YES",
-            "use_localtime=YES",
-            "xferlog_enable=YES",
-            "connect_from_port_20=YES",
-            "chroot_local_user=YES",
-            "secure_chroot_dir=/var/run/vsftpd/empty",
-            "pam_service_name=vsftpd",
-            "rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem",
-            "rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key",
-            "ssl_enable=NO",
-            "pasv_enable=Yes",
-            "pasv_min_port=10000",
-            "pasv_max_port=10100",
-            "allow_writeable_chroot=YES",
-            # ... more options as needed
+            "anonymous_enable=YES", # Allows anonymous users to access the FTP server
+            "listen=YES",           # Instructs vsftpd to run in standalone mode
+            "use_localtime=YES",    # Configures vsftpd to display timestamps according to the server's local timezone
+            "pasv_enable=Yes",      # Enables passive mode - necessary for clients behind firewalls or NAT
+            f"listen_port={ftpPort}",    # Sets the port for vsftpd to listen on
+            f"local_root={directory}",   # Defines the root directory for FTP users
+            f"listen_address={bindAddr}" # Sets the specific IP address for vsftpd to bind to
         ]
+        # Joins all configuration lines into a single string separated by newline characters
         return "\n".join(config_lines)
 
-    def install_location(self: Self) -> Path:
+    def install_location(self) -> Path:
         """Returns the location that the vsftpd config file should be installed to."""
+        # Provides the path to the configuration file where vsftpd expects to find it
         return Path("/etc/vsftpd.conf")
+
+    def setup_commands(self) -> List[str] | List[List[str]]:
+        return[ "systemctl restart vsftpd.service" ]
