4c7e85a6bc41dc9e8150a2c6bbb761099301566d
Bobzemob <89527529+HenrithicusGreenson@users.noreply.github.com>
2023-11-06 20:51:09 -0500

    Changing linter complaints
    Addressed several linter complaints:
        Reduced line length by shortening comments and breaking down longer lines.
        Used class-level comments (''' ''') for the class description.
        Addressed missing timeout argument in requests.get to prevent indefinite hanging.
        Reordered imports according to the linting suggestions.
        Corrected indentation issues.
        Added a final newline at the end of the file as recommended by the linter.

--- genisys/modules/os_file_download.py
+++ genisys/modules/os_file_download.py
@@ -1,35 +1,31 @@
-'''
-Notes for self: 
-This module will handle the downloading of all of the OS files necessary for the PXE booting of a client machine. 
-
-- vmlinuz (Kernel) & initrd/initramfs (Initial Ram Disk)
-- pxelinux.cfg 
-- pxelinux.0 or GRUB (Depends on OS/Version)
-
-Link to Debian files (amd64): https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/ 
-'''
-from genisys.modules import base
 from pathlib import Path
 from typing_extensions import Self
 import requests
 import tarfile
 import os
+from genisys.modules import base
 
 class OSDownload(base.Module):
-    DEBIAN_TAR_FILE_LINK = "https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/netboot.tar.gz" 
+    """
+    This module handles the downloading of OS files necessary for PXE booting a client machine.
+    - vmlinuz (Kernel) & initrd/initramfs (Initial Ram Disk)
+    - pxelinux.cfg
+    - pxelinux.0 or GRUB (Depends on OS/Version)
+    Link to Debian files (amd64): https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/
+    """
+
+    DEBIAN_TAR_FILE_LINK = "https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/netboot/netboot.tar.gz"
     DEBIAN_TAR_FILENAME = "netboot.tar.gz"
 
     def __init__(self: Self, config) -> None:
-       self.config = config
-       self.config["network"] = config["Network"]
-
-    # end __init__
+        self.config = config
+        self.config["network"] = config["Network"]
 
     def install(self: Self, chroot: Path = ...):
         tftp_directory = self.config["network"]["tftp_directory"]
 
         try:
-            response = requests.get(self.DEBIAN_TAR_FILE_LINK, allow_redirects=True)
+            response = requests.get(self.DEBIAN_TAR_FILE_LINK, allow_redirects=True, timeout=15)
             response.raise_for_status()  # Check for request errors
 
             tmp_directory = Path("/tmp")
@@ -50,6 +46,5 @@ class OSDownload(base.Module):
             print(f"Request failed: {e}")  # Handle request exceptions
         except (OSError, tarfile.TarError) as e:
             print(f"Error: {e}")  # Handle file and extraction errors
-    #end install
 
-# end OSDownload
+# Ensure a newline at the end of the file
