e36e307d9e27fb629e7f45b0cc351d9f4fc09a3e
Bobzemob <greeht01@pfw.edu>
2023-11-08 18:01:18 -0500

    Reformatted code into several methods

--- genisys/modules/os_file_download.py
+++ genisys/modules/os_file_download.py
@@ -5,10 +5,9 @@ from genisys.modules import base
 
 class OSDownload(base.Module):
     """
-    This module downloads the Netboot.xyz PXE file, and (maybe? might be automated buy a different module) 
-    automates interacting with the Netboot UI to select an os. 
+    This module downloads the Netboot.xyz PXE file and potentially interacts with the Netboot UI to select an OS.
     
-    Link to Netboot.xyz pxe file: https://boot.netboot.xyz/ipxe/netboot.xyz.kpxe
+    Link to Netboot.xyz PXE file: https://boot.netboot.xyz/ipxe/netboot.xyz.kpxe
     """
 
     def __init__(self: Self, config) -> None:
@@ -19,28 +18,36 @@ class OSDownload(base.Module):
         self.NETBOOT_PXE_LINK = "https://boot.netboot.xyz/ipxe/netboot.xyz.kpxe"
     # end __init__
 
-    def install(self: Self, chroot: Path = ...):
-        tftp_directory = self.config["network"]["tftp_directory"]
-
+    def download_file(self: Self) -> bytes:
         try:
             response = requests.get(self.NETBOOT_PXE_LINK, allow_redirects=True, timeout=15)
             response.raise_for_status()  # Check for request errors
+            return response.content
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")  # Handle request exceptions
+            return b''  # Return an empty bytes object to indicate download failure
+    # end download_file
 
+    def write_file(self: Self, content: bytes, download_dir: Path):
+        try:
+            download_dir.parent.mkdir(parents=True, exist_ok=True)  # Ensure directory exists
+            with open(download_dir, "wb") as file:
+                file.write(content)
+        except (FileNotFoundError, PermissionError, IOError) as e:
+            print(f"File writing error: {e}")  # Handle file writing exceptions
+    # end write_file
+
+    def install(self: Self, chroot: Path = ...):
+        tftp_directory = self.config["network"]["tftp_directory"]
+        content = self.download_file()
+
+        if content:
             if chroot:
                 download_dir = chroot / tftp_directory / self.NETBOOT_FILENAME
             else:
                 download_dir = tftp_directory / self.NETBOOT_FILENAME
 
-
-            try:
-                with open(download_dir, "wb") as file:
-                    file.write(response.content)
-                    
-            except (FileNotFoundError, PermissionError, IOError) as e:
-                print(f"File writing error: {e}")  # Handle file writing exceptions
-
-        except requests.RequestException as e:
-            print(f"Request failed: {e}")  # Handle request exceptions
-    # end install 
+            self.write_file(content, download_dir)
+    # end install
 
 # end OSDownload
