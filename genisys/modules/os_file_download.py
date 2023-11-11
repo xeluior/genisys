07f1d7bf412657b61557e29a0866f672a4464b6e
Bobzemob <89527529+HenrithicusGreenson@users.noreply.github.com>
2023-11-11 15:39:50 -0500

    Revert "Changed module to use Netboot.xyz"
    This reverts commit 0ac36747adbadb4ae7849146754f3b52039a5540.

--- genisys/modules/os_file_download.py
+++ genisys/modules/os_file_download.py
@@ -1,46 +1,52 @@
 from pathlib import Path
 from typing_extensions import Self
 import requests
+import tarfile
+import os
 from genisys.modules import base
 
 class OSDownload(base.Module):
     """
-    This module downloads the Netboot.xyz PXE file, and (maybe? might be automated buy a different module) 
-    automates interacting with the Netboot UI to select an os. 
-    
-    Link to Netboot.xyz pxe file: https://boot.netboot.xyz/ipxe/netboot.xyz.kpxe
+    This module handles the downloading of OS files necessary for PXE booting a client machine.
+    - vmlinuz (Kernel) & initrd/initramfs (Initial Ram Disk)
+    - pxelinux.cfg
+    - pxelinux.0 or GRUB (Depends on OS/Version)
+    Link to Debian files (amd64): https://deb.debian.org/debian/dists/bookworm/main/installer-amd64/current/images/
     """
 
     def __init__(self: Self, config) -> None:
         self.config = config
         self.config["network"] = config["Network"]
 
-        self.NETBOOT_FILENAME = "netboot.xyz.kpxe"
-        self.NETBOOT_PXE_LINK = "https://boot.netboot.xyz/ipxe/netboot.xyz.kpxe"
+        self.DEBIAN_TAR_FILENAME = "netboot.tar.gz"
+        self.DEBIAN_TAR_LINK = f"https://deb.debian.org/debian/dists/{self.config['version-name']}/main/installer-{self.config['target-architecture']}/current/images/netboot/netboot.tar.gz"
     # end __init__
 
     def install(self: Self, chroot: Path = ...):
         tftp_directory = self.config["network"]["tftp_directory"]
 
         try:
-            response = requests.get(self.NETBOOT_PXE_LINK, allow_redirects=True, timeout=15)
+            response = requests.get(self.DEBIAN_TAR_LINK, allow_redirects=True, timeout=15)
             response.raise_for_status()  # Check for request errors
 
-            if chroot:
-                download_dir = chroot / tftp_directory / self.NETBOOT_FILENAME
-            else:
-                download_dir = tftp_directory / self.NETBOOT_FILENAME
+            tmp_directory = Path("/tmp")
+            download_dir = tmp_directory / self.DEBIAN_TAR_FILENAME
 
+            with open(download_dir, "wb") as file:
+                file.write(response.content)
 
-            try:
-                with open(download_dir, "wb") as file:
-                    file.write(response.content)
-                    
-            except (FileNotFoundError, PermissionError, IOError) as e:
-                print(f"File writing error: {e}")  # Handle file writing exceptions
+            target_directory = Path(chroot, tftp_directory) if chroot else Path(tftp_directory)
+
+            with tarfile.open(download_dir, "r:gz") as tar:
+                tar.extractall(target_directory)
+
+            # Remove the downloaded .tar.gz file from /tmp
+            os.remove(download_dir)
 
         except requests.RequestException as e:
             print(f"Request failed: {e}")  # Handle request exceptions
+        except (OSError, tarfile.TarError) as e:
+            print(f"Error: {e}")  # Handle file and extraction errors
     # end install 
 
 # end OSDownload
