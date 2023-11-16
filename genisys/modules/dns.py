07758d44dd5dab051489b6cbed6c2e0c08ec3b0f
D-Patel43 <patedm01@pfw.edu>
2023-11-16 16:03:19 -0500

    Changed generate function return to a String along with reading information from both network and DNSMasq Overrides sections of the config file

--- genisys/modules/dns.py
+++ genisys/modules/dns.py
@@ -7,23 +7,34 @@ class Dnsmasq(base.Module):
     DNS_DIR = '/etc'
     DNS_FILE = 'dnsmasq.conf'
     def __init__(self: Self, config):
-        """Temporarily seeing if this portion of the config file is applicable to DNSMasq"""
-        self.config = config.getSection("DNSMasq Overrides")
+        """Pulling DNSMasq config information from the Network header in the config yaml file"""
+        self.config["network"] = config.getSection("Network")
+        #Adding override config to read the last field in our config file
+        self.config["overrides"] = config.getSection("DNSMasq Overrides")
     def install_location(self: Self) -> Path:
         """This is where the DNSMasq config file is stored/accessed"""
         return Path(DNS_DIR, DNS_FILE)
     def generate(self: Self) -> str:
-        #consider adding DNSStubListener=no to /etc/systemd/resolved.conf to prevent port 53 errors?
-        backup = Path(DNS_DIR, 'dnsmasq.conf.orig')
-        if(not backup.exists()):
-            subprocess.run("sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig")
-        configWriter = open(DNS_DIR + DNS_FILE, "w")
-        if 'authoritative' in self.config:
-            if self.config['authoritative'].lower() == 'true':
-                configWriter.write("dhcp-authoritative\n")
+        configWriter = ''
+        if 'interface' in self.config["network"]:
+            configWriter+=("interface=" + self.config['network']['interface'] + "\n")
+        if 'no-dhcp' in self.config["network"]:
+            if self.config['network']['no-dhcp'] == 'false':
+                if 'dhcp-ranges' in self.config['network'] and 'dhcp-lease' in self.config['network']:
+                    configWriter+=("dhcp-range=" + self.config['network']['dhcp-ranges'] + "," + self.config['network']['dhcp-lease'] + "\n")
+        configWriter+="enable-tftp\n"
+        if 'tftp_directory' in self.config['netowrk']:
+            configWriter+=("tftp-root=" + self.config['network']['tftp_directory'] + "\n")
+        if 'dns-servers' in self.config['network']:
+            configWriter+=("server=" + self.config['network']['dns-servers'] + "\n")
+        #adding potential future logic for disabling only dns below
+        # if 'no-dns' in self.config:
+        #     We add the following string: DNSMASQ_EXCEPT=lo to the file /etc/default/dnsmasq 
+        if 'authoritative' in self.config['overrides']:
+            if self.config['overrides']['authoritative'].lower() == 'true':
+                configWriter+="dhcp-authoritative\n"
             else:
-                configWriter.write("#dhcp-authoritative\n")
-        configWriter.close()
+                configWriter+="#dhcp-authoritative\n"
     def setup_commands(self: Self) -> Union[List[str], List[List[str]]]:
         return ["systemctl restart dnsmasq"]
     def validate(self: Self) -> bool:
