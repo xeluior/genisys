fa8cb1a1fb09375185b7dc1172917ae4dc44bc21
Henry Green <greeht01@pfw.edu>
2024-01-12 12:44:10 -0500

    Started move_files function

--- genisys/modules/script.py
+++ genisys/modules/script.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from typing_extensions import Self
+from os import rename
 
 from genisys.modules.base import Module
 from genisys.config_parser import YAMLParser
@@ -7,7 +9,22 @@ class Script(Module):
 
     def __init__(self: Self, config: YAMLParser):
         self.config = {}
-        self.config["Scripts"] =  config.get_section("Scripts")
+        self.config["scripts"] = config.get_section("Scripts")
+        self.config["network"] = config.get_section("Network")
     #end __init__
+
+    def move_files(self: Self):
+        #Location of scripts in genisys directory
+        source = Path("/genisys/scripts") #This will need fixed 
+        #Location of FTP directory on genisys host 
+        destination = Path(self.config["network"]["ftp"]["directory"])
         
-    
+        rename(source, destination)
+    #end move_files
+
+# def main():
+#     script = Script(YAMLParser('C:/Users/greeht01/Desktop/genisys/documentation/example.yml'))
+#     script.move_files()
+
+# if __name__ == '__main__':
+#     main()
