e7637ace8f70e77fcd28dee42ece415cfa808b31
Robert Gingras <developer@three-point-five.dev>
2024-01-25 10:25:31 -0500

    fixed install path

--- genisys/modules/firstboot.py
+++ genisys/modules/firstboot.py
@@ -5,14 +5,16 @@ from genisys.config_parser import YAMLParser
 
 class Service(Module):
     """Installs the firstboot systemd service file to the ftp dir to be downloaded by clients"""
+    SERVICE_NAME = "genisys-firstboot.service"
+
     def __init__(self: Self, config: YAMLParser):
         self.config = {}
         self.config["path"] = config.get_section("Network").get("ftp", {}).get("directory")
 
     def install_location(self: Self) -> Path:
-        return Path(self.config["path"])
+        return Path(self.config["path"]) / "first-boot" / self.SERVICE_NAME
 
     def generate(self: Self):
-        template_path = Path(__file__).parent / "templates" / "genisys-firstboot.service"
+        template_path = Path(__file__).parent / "templates" / self.SERVICE_NAME
         with template_path.open("r") as fd:
             return fd.read()
