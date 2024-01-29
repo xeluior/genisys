776c8d49f016b548fa790944141ff3a288ceb113
Henry Green <greeht01@pfw.edu>
2024-01-29 15:14:51 -0500

    Wrote generate function with POST as transmission method

--- genisys/modules/firstboot.py
+++ genisys/modules/firstboot.py
@@ -6,20 +6,35 @@ from genisys.config_parser import YAMLParser
 
 class FirstBoot(Module):
     """
-    The FirstBoot class generates a shell script that will be 
-    sent to the client which will send a POST request to the 
+    The FirstBoot class generates a shell script for the 
+    client machines which sends a POST request to the 
     Genisys server containing the IP address of the client 
     machine for use in the Ansible inventory file. 
     """
 
     def __init__(self: Self, config: YAMLParser):
-        pass
+        self.config = {}
+        self.config["scripts"] = config.get_section("Scripts")
+        self.config["network"] = config.get_section("Network")
     #end __init__
 
     def generate(self: Self):
-        pass
+        content = []
+
+        # Shell command to get IP address
+        content.append("ipaddr=$(hostname -I)")
+
+        # Command to send POST request to Genisys server (Subject to change)
+        curl_command = "curl -X POST -d \"$(ipaddr)\" " + self.config["network"]["ip"]
+        content.append(curl_command)
+
+        return content
     #end generate
 
     def install_location(self: Self) -> Path:
         pass
     #end install_location
+
+    def install(self: Self, chroot: Path = Path('/')):
+        pass
+    #end install
