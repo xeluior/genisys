d7453a3e176c66dc3ebeca4f0bc616d1793c0341
Robert Gingras <developer@three-point-five.dev>
2024-02-01 15:34:24 -0500

    install sigterm handler for server process

--- genisys/server/__init__.py
+++ genisys/server/__init__.py
@@ -4,13 +4,18 @@ import ssl
 import pwd
 import grp
 import os
+import sys
+import json
+import textwrap
 from warnings import warn
+from signal import signal, SIGTERM
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from typing_extensions import Self, Dict, TypedDict, cast
 from genisys.config_parser import YAMLParser
 import genisys.server.tls
 
 DEFAULT_PORT = 15206
+DEFAULT_INVENTORY = "/etc/ansible/hosts"
 
 class ServerOptions(TypedDict):
     port: int
@@ -32,6 +37,10 @@ class GenisysHTTPRequestHandler(BaseHTTPRequestHandler):
 
         Success"""), encoding='utf-8'))
 
+class GenisysHTTPServer(HTTPServer):
+    """HTTP Server wrapper to allow server global variables to be used for processing requests"""
+    inventory_file: TextIOWrapper
+
 def run(config: YAMLParser):
     """Drops priviledges, creates the server (with SSL, if applicable), then waits for requests"""
     # parse config
@@ -45,7 +54,7 @@ def run(config: YAMLParser):
     # create a server
     server_address = network.get('ip', '')
     server_port = server_options.get("port", DEFAULT_PORT)
-    httpd = HTTPServer((server_address, server_port), GenisysHTTPRequestHandler)
+    httpd = GenisysHTTPServer((server_address, server_port), GenisysHTTPRequestHandler)
 
     # apply TLS if applicable
     if 'ssl' in server_options:
@@ -55,7 +64,17 @@ def run(config: YAMLParser):
         ssl_context.load_cert_chain(**ssl_cert)
         httpd.socket = ssl_context.wrap_socket(httpd.socket)
 
-    # listen
+    # install additional data for the server to use
+    ansible_cfg = config.get_section("ansible")
+    inventory_path = ansible_cfg.get("inventory", DEFAULT_INVENTORY)
+    httpd.inventory_file = open(inventory_path, 'w', encoding='utf-8')
+
+    # run until SIGTERM is caught
+    def sigterm_handler(*_):
+        print("killed")
+        httpd.inventory_file.close()
+        sys.exit(SIGTERM)
+    signal(SIGTERM, sigterm_handler)
     httpd.serve_forever()
 
 def drop_priviledges(config: ServerOptions) -> bool:
