c226c40b5d49645cfc0e52c218070e698fa30013
Henry Green <greeht01@pfw.edu>
2024-02-05 16:44:28 -0500

    Remove newline

--- genisys/modules/firstboot/hello.py
+++ genisys/modules/firstboot/hello.py
@@ -7,7 +7,7 @@ from genisys.config_parser import YAMLParser
 
 class Hello(Module):
     """
-    The FirstBoot class generates a shell script for the 
+    The Hello class generates a shell script for the 
     client machines which sends a POST request to the 
     Genisys server containing the IP address and hostname 
     of the client machine for use in the Ansible inventory file. 
@@ -33,7 +33,6 @@ class Hello(Module):
         # Command to send POST request to Genisys server (Subject to change)
         curl_command = "curl -X POST -H \"Content-Type: application/json\" -d" + "\"" + json_object + "\"" + self.config["network"]["ip"]
         content.append(curl_command)
-
 
         formatted_string = "\n".join(content)
         return formatted_string
