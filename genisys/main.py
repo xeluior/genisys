f88f7e56dc6c8a3700737df98693a46d57e19be9
Henry Green <greeht01@pfw.edu>
2024-02-05 16:29:05 -0500

    Added module imports to main

--- genisys/main.py
+++ genisys/main.py
@@ -10,8 +10,9 @@ from genisys.modules.dns import Dnsmasq
 from genisys.modules.ftp import VsftpdModule
 from genisys.modules.os_file_download import OSDownload
 from genisys.modules.syslinux import Syslinux
-from genisys.modules.firstboot import Service
 from genisys.modules.script import Script
+from genisys.modules.firstboot.hello import Hello
+from genisys.modules.firstboot.service import Service
 import genisys.config_parser as cp
 
 MODULES = [
@@ -23,7 +24,8 @@ MODULES = [
     Dnsmasq,
     VsftpdModule,
     Syslinux,
-    Service
+    Hello, 
+    Service,
     Script
 ]
 
