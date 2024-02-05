44acbc34f132b3feb22115e818b1117f33b56fd2
Henry Green <greeht01@pfw.edu>
2024-02-05 16:19:06 -0500

    Fixed the BS caused by rebase
    Inshallah I didn't change anything outside of these modules

--- genisys/modules/firstboot/hello.py
+++ genisys/modules/firstboot/hello.py
@@ -1,20 +1,47 @@
+import json
 from pathlib import Path
 from typing_extensions import Self
 from genisys.modules.base import Module
 from genisys.config_parser import YAMLParser
 
-class Service(Module):
-    """Installs the firstboot systemd service file to the ftp dir to be downloaded by clients"""
-    SERVICE_NAME = "genisys-firstboot.service"
-
+class FirstBoot(Module):
+    """
+    The FirstBoot class generates a shell script for the 
+    client machines which sends a POST request to the 
+    Genisys server containing the IP address and hostname 
+    of the client machine for use in the Ansible inventory file. 
+    """
     def __init__(self: Self, config: YAMLParser):
         self.config = {}
-        self.config["path"] = config.get_section("Network").get("ftp", {}).get("directory")
-
-    def install_location(self: Self) -> Path:
-        return Path(self.config["path"]) / "first-boot" / self.SERVICE_NAME
+        self.config["scripts"] = config.get_section("Scripts")
+        self.config["network"] = config.get_section("Network")
+    #end __init__
 
     def generate(self: Self):
-        template_path = Path(__file__).parent / "templates" / self.SERVICE_NAME
-        with template_path.open("r") as fd:
-            return fd.read()
+        content = []
+        json_body = {
+            'message' : 'hello', 
+            'ip' : '$(hostname -I)',
+            'hostname' : '$(hostname)'
+        }
+        # Not sure of json.dumps() is necessary, will need testing
+        json_object = json.dumps(json_body)
+
+        content.append("#!/bin/bash")
+
+        # Shell command to get IP address
+        content.append("ipaddr=$(hostname -I)")
+
+        # Command to send POST request to Genisys server (Subject to change)
+        curl_command = "curl -X POST -H \"Content-Type: application/json\" -d" + "\"" + json_object + "\"" + self.config["network"]["ip"]
+        content.append(curl_command)
+        return content
+    #end generate
+
+    def install_location(self: Self) -> Path:
+        return "/first-boot/"
+    #end install_location
+
+    def install(self: Self, chroot: Path = Path('/')):
+        pass
+    #end install
