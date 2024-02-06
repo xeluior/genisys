a90f10a2b1cde45564a84857906a754b72340567
Henry Green <greeht01@pfw.edu>
2024-02-06 14:42:32 -0500

    Added second test

--- tests/modules/test_hello.py
+++ tests/modules/test_hello.py
@@ -18,3 +18,18 @@ class HelloModuleTest(unittest.TestCase):
             output = module.generate().split("\n")
             for line in expected_output_list:
                 self.assertIn(line, output)
+
+    def test_ssl_config_values_not_present(self):
+        """Test that the ssl option is correctly detected, opposite of test 1"""
+        expected_output_list = [
+            "#!/bin/bash",
+            "curl -X POST -H 'Content-Type: application/json' -d {\"message\": \"hello\", \"ip\": \"$(hostname -I)\", \"hostname\": \"$(hostname)\"} 10.0.0.1:15206"
+        ]
+
+        with open("tests/configs/hello_test_2.yml", encoding='utf-8') as config_file:
+            config = config_parser.YAMLParser(config_file.name)
+            module = Hello(config)
+
+            output = module.generate().split("\n")
+            for line in expected_output_list:
+                self.assertIn(line, output)
