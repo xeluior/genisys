43aff0e131bc904e21c2629bf6975da9e0aee071
Henry Green <greeht01@pfw.edu>
2024-02-06 14:27:59 -0500

    Test 1

--- tests/modules/test_hello.py
+++ tests/modules/test_hello.py
@@ -4,4 +4,13 @@ from genisys.modules.firstboot.hello import Hello
 from genisys import config_parser
 
 class HelloModuleTest(unittest.TestCase):
-"""Tests for the Hello module"""
+    """Tests for the Hello module"""
+    def test_ssl_config_values_present(self):
+        """Test that the ssl option is correctly detected"""
+        with open("tests/configs/hello_test_1.yml", encoding='utf-8') as config_file:
+            config = config_parser.YAMLParser(config_file.name)
+            module = Hello(config)
+
+            output = module.generate()
+            print(output)
+            
