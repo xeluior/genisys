e6d250aa3dab7636bf12599e92774152ace6c165
Henry Green <greeht01@pfw.edu>
2024-02-06 14:35:48 -0500

    Fixed test 1

--- tests/modules/test_hello.py
+++ tests/modules/test_hello.py
@@ -1,5 +1,4 @@
 import unittest
-from pathlib import Path
 from genisys.modules.firstboot.hello import Hello
 from genisys import config_parser
 
@@ -7,10 +6,15 @@ class HelloModuleTest(unittest.TestCase):
     """Tests for the Hello module"""
     def test_ssl_config_values_present(self):
         """Test that the ssl option is correctly detected"""
+        expected_output_list = [
+            "#!/bin/bash",
+            "https://10.0.0.1:15206"
+        ]
+
         with open("tests/configs/hello_test_1.yml", encoding='utf-8') as config_file:
             config = config_parser.YAMLParser(config_file.name)
             module = Hello(config)
 
-            output = module.generate()
-            print(output)
-            
+            output = module.generate().split("\n")
+            for line in expected_output_list:
+                self.assertIn(line, output)
