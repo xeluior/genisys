cca4021f5ccd55c00eeb68d3070231254590e8bb
Henry Green <greeht01@pfw.edu>
2024-02-15 14:00:24 -0500

    Create genisysinventory.py

---
+++ genisys/server/genisysinventory.py
@@ -0,0 +1,10 @@
+from typing_extensions import Self
+
+
+class GenisysInventory:
+    """Handles operations relating to the creation of a temporary inventory
+    file of all of the client machines booted through Genisys, and to store 
+    metadata associated with each client"""
+
+    def __init__(self: Self):
+        pass
