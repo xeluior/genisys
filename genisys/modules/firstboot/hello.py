8c92d095bd653dde4695c9185958ea61e34a4079
HenrithicusGreenson <89527529+HenrithicusGreenson@users.noreply.github.com>
2024-02-15 08:19:37 -0500

    First boot script hello (#51)
    * Create firstboot.py
    
    * Wrote generate function with POST as transmission method
    
    * Added shebang to script
    
    * Added port config to YAML file
    
    * Add genisys-firstboot.service for launching first boot script (#43)
    
    * added service file
    
    * added simple module to "install" the service file to the FTP dir
    
    * added new module to main
    
    * fixed lineter error
    
    * fixed install path
    
    * Ansible (#44)
    
    * added playbooks for ansible
    
    * Added j2 template for install docker and then selenium
    
    * Rename template.j2 to docker_selinum.yml
    
    * updated README with new prereq and some competitor listing (#49)
    
    * updated README with new prereq and some competitor listing
    
    * added puppet
    
    * Added JSON object to POSt request, added install_location
    
    * Removed redundant/unused var
    
    * Created sub directory
    
    * Added port config to YAML file
    
    * Created sub directory
    
    * Fixed the BS caused by rebase
    
    Inshallah I didn't change anything outside of these modules
    
    * Added module imports to main
    
    * Reformatted from list of strings to single string
    
    * Remove newline
    
    * Change Requests 1
    
    * Removed "Install()" to use default behavior
    * Updated "Install_Path()" to return correct file path.
    
    * Fixed curl_command formatting
    
    * Created tests file, updated old tests
    
    * Test 1
    
    * Fixed casting bug
    
    * Fixed test 1
    
    * Fixed test bug 2
    
    * Test fix 3
    
    * Added second test
    
    * Removed comment
    
    * Added robert's parsing fix
    
    * Fixed test cases
    
    * Fixed yaml file setup
    
    * Changed working directory in service file
    
    * Updated curl command and test cases
    
    * Final script fix (hopefully)
    
    ---------
    
    Co-authored-by: Robert <developer@three-point-five.dev>
    Co-authored-by: Benny Bergle <58752312+BBergle@users.noreply.github.com>

---
+++ genisys/modules/firstboot/hello.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from typing_extensions import Self
+
+from genisys.modules.base import Module
+from genisys.config_parser import YAMLParser
+
+class Hello(Module):
+    """
+    The Hello class generates a shell script for the 
+    client machines which sends a POST request to the 
+    Genisys server containing the IP address and hostname 
+    of the client machine for use in the Ansible inventory file. 
+    """
+    def __init__(self: Self, config: YAMLParser):
+        self.config = {}
+        self.config["scripts"] = config.get_section("Scripts")
+        self.config["network"] = config.get_section("Network")
+    #end __init__
+
+    def generate(self: Self):
+        content = []
+
+        content.append("#!/bin/bash")
+
+        #Shell Variables
+        content.append("ip_addr=$(hostname -I)")
+        content.append("hostname=$(hostname)")
+
+        #Writing JSON content to file
+        append_string = " >> ip.json"
+
+        content.append("echo \"{\" > ip.json") #single > to overwrite
+        content.append("echo \"    \\\"message\\\" : \\\"hello\\\",\"" + append_string)
+        content.append("echo \"    \\\"ip\\\" : \\\"$ip_addr\\\",\"" + append_string)
+        content.append("echo \"    \\\"hostname\\\" : \\\"$hostname\\\",\"" + append_string)
+        # Additional JSON content can be added on this line
+        content.append("echo \"}\"" + append_string)
+
+        # Building target IP for curl request
+        server_config = self.config["network"].get("server", {}) or {}
+        prefix = "https://" if "ssl" in server_config else ""
+        server_ip = self.config["network"]["ip"]
+        server_port = self.config["network"]["server"]["port"]
+        target_ip = prefix + server_ip + ":" + str(server_port)
+
+        # Command to send POST request to Genisys server (Subject to change)
+        curl_command = f"curl --json @ip.json {target_ip}"
+        content.append(curl_command)
+
+        # Turning array of strings into single block
+        formatted_string = "\n".join(content)
+        return formatted_string
+    #end generate
+
+    def install_location(self: Self) -> Path:
+        return Path(self.config["network"]["ftp"]["directory"]) / "first-boot" / "entrypoint"
+    #end install_location
+#end Hello
