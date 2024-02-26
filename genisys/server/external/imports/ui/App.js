788d91bfa4d0c0162192d530d9b46018975a83b9
Henry Green <greeht01@pfw.edu>
2024-02-26 15:45:39 -0500

    Following MeteorJS Blaze tutorial
    https://blaze-tutorial.meteor.com/simple-todos/02-collections

---
+++ genisys/server/external/imports/ui/App.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { TasksCollection } from "../api/TasksCollection";
+import './App.html';
+
+Template.mainContainer.helpers({
+  tasks() {
+    return TasksCollection.find({}).fetch();
+  },
+});
