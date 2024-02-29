cd60745f57fa0011aa0c212835c2e59d8e6bdb63
r-akers <60190206+r-akers@users.noreply.github.com>
2024-02-29 17:39:57 -0500

    make class

--- genisys/make_password.py
+++ genisys/make_password.py
@@ -1,6 +1,21 @@
 import subprocess
+from passlib.hash import pbkdf2_sha256
+from genisys.config_parser import YAMLParser
 
-def make_password(raw_password):
-    secret_password = ""
-    # encrypt raw_password from stdin stored as secret_password in file etc/shadow
-    subprocess.run(["echo", "-n", raw_password, "ansible-vault", "encrypt_string", "--vault-password-file", "etc/shadow", "--stdin-name", secret_password], check=False)
+class MakePassword:
+    """ Accepts command line argument to generate user password """
+    def __init__(self, config: YAMLParser):
+        self.config = {}
+        self.config["users"] = config.get_section("Users")
+    # end __init__
+     
+    def make_password(self, raw_password):
+        """ Encrypts user password in hashed format and ansible vault format """
+
+        # use Passlib library to encrypt raw_ password, in /etc/shadow format in the "root-password" key under the "Users" section of the genisys config.
+        secret_password = pbkdf2_sha256.hash(raw_password)
+        self.config["users"]["root-login"] = secret_password
+
+        # encrypt raw_password from stdin using ansible vault, stored as secret_password in file etc/shadow
+        user_password = ""
+        subprocess.run(["echo", "-n", raw_password, "ansible-vault", "encrypt_string", "--vault-password-file", "etc/shadow", "--stdin-name", user_password], check=False)
