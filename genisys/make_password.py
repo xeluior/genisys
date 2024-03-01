1a86443e9f072a61d691c8666f641525c1822e15
r-akers <60190206+r-akers@users.noreply.github.com>
2024-02-29 19:05:11 -0500

    specified file option

--- genisys/make_password.py
+++ genisys/make_password.py
@@ -9,13 +9,12 @@ class MakePassword:
         self.config["users"] = config.get_section("Users")
     # end __init__
      
-    def make_password(self, raw_password):
+    def make_password(self, raw_password, filename):
         """ Encrypts user password in hashed format and ansible vault format """
-
         # use Passlib library to encrypt raw_ password, in /etc/shadow format in the "root-password" key under the "Users" section of the genisys config.
         secret_password = pbkdf2_sha256.hash(raw_password)
         self.config["users"]["root-login"] = secret_password
 
-        # encrypt raw_password from stdin using ansible vault, stored as secret_password in file etc/shadow
+        # encrypt raw_password from stdin using ansible vault, stored as secret_password in file specified file
         user_password = ""
-        subprocess.run(["echo", "-n", raw_password, "ansible-vault", "encrypt_string", "--vault-password-file", "etc/shadow", "--stdin-name", user_password], check=False)
+        subprocess.run(["echo", "-n", raw_password, "ansible-vault", "encrypt_string", "--vault-password-file", filename, "--stdin-name", user_password], check=False)
