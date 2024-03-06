e079e8fd338b657959a27fa4620e76154a1773e2
Robert Gingras <gingrm02@pfw.edu>
2024-03-06 14:09:02 -0500

    added recursive copy for test data sub directories

--- tests/scripts/test.sh
+++ tests/scripts/test.sh
@@ -87,7 +87,7 @@ poetry build --format=wheel
 mv './dist/genisys-0.1.0-py3-none-any.whl' "${SHARED_FOLDER}"
 
 # add additional test files to the shared folder
-cp "${TEST_PATH}/data"/* "${SHARED_FOLDER}"
+cp -r "${TEST_PATH}/data"/* "${SHARED_FOLDER}"
 
 # cache the template VDI download
 if [ ! -r "${TEMPLATE_VDI_CACHE_FILE}" ]; then
