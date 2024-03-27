586c13ee6fec09161c3107bfe5069b2dc5c2c2f1
Henry Green <greeht01@pfw.edu>
2024-03-27 15:54:54 -0400

    Changed formatting

--- meteor-dev/api/clients/server/methods.js
+++ meteor-dev/api/clients/server/methods.js
@@ -1,65 +1,60 @@
-import {
-    Meteor
-} from 'meteor/meteor';
-import {
-    check
-} from 'meteor/check';
-import { ClientsCollection } from '../clients';
-import fs from 'fs';
+import { Meteor } from "meteor/meteor"
+import { check } from "meteor/check"
+import { ClientsCollection } from "../clients"
+import fs from "fs"
 
 Meteor.methods({
-    'Clients.Provision': function (clientId) {
-        check(clientId, String);
+  "Clients.Provision": function (clientId) {
+    check(clientId, String)
 
-        const client = ClientsCollection.findOne({
-            _id: clientId
-        });
+    const client = ClientsCollection.findOne({
+      _id: clientId,
+    })
 
-        if (!client) {
-            throw new Meteor.Error('client-not-found', 'That client doesn\'t exist.');
-        }
-
-        fs.access('inventory', fs.constants.F_OK, (err) => {
-            if (err) {
-                console.log(`inventory does not exist`);
-                fs.writeFileSync('inventory', '');
-            } else {
-                console.log(`inventory exists`);
-            }
-        });
-
-        fs.readFileSync('inventory', function (file) {
-            // check if client.hostname exists in file, do nothing
-            // if client.hostname is not in file, add to the end of the file and write the file out.
-            console.log('file', file);
-        });
-
-
-
-        ClientsCollection.update({
-            _id: clientId,
-        }, {
-            $set: {
-                provisioned: true,
-                provisionedAt: new Date()
-                // provisionedBy: Meteor.user()
-            }
-        });
-        return {
-            status: 200,
-            message: `${client.hostname} successfully provisioned`
-        };
-
-    },
-    'TestYaml': function () {
-        const exampleYaml = Assets.get('example.yml.tpl');
-
-        const replace = {
-            interface: 'eth0',
-            subnet: '10.0.0.0/24'
-        }
-        // replace {{key}} with value
-        // fs.writeFile...
-
+    if (!client) {
+      throw new Meteor.Error("client-not-found", "That client doesn't exist.")
     }
-});
+
+    fs.access("inventory", fs.constants.F_OK, (err) => {
+      if (err) {
+        console.log(`inventory does not exist`)
+        fs.writeFileSync("inventory", "")
+      } else {
+        console.log(`inventory exists`)
+      }
+    })
+
+    fs.readFileSync("inventory", function (file) {
+      // check if client.hostname exists in file, do nothing
+      // if client.hostname is not in file, add to the end of the file and write the file out.
+      console.log("file", file)
+    })
+
+    ClientsCollection.update(
+      {
+        _id: clientId,
+      },
+      {
+        $set: {
+          provisioned: true,
+          provisionedAt: new Date(),
+          // provisionedBy: Meteor.user()
+        },
+      }
+    )
+    return {
+      status: 200,
+      message: `${client.hostname} successfully provisioned`,
+    }
+  },
+  TestYaml: function () {
+    const exampleYaml = Assets.get("example.yml.tpl")
+
+    const replace = {
+      interface: "eth0",
+      subnet: "10.0.0.0/24",
+    }
+    // replace {{key}} with value
+    // fs.writeFile...
+  },
+})
--- meteor-dev/imports/ui/App.js
+++ meteor-dev/imports/ui/App.js
@@ -1,32 +1,35 @@
-import { Template } from 'meteor/templating';
-import './App.html';
-import { ClientsCollection } from '../../api/clients/clients';
+import { Template } from "meteor/templating"
+import "./App.html"
+import { ClientsCollection } from "../../api/clients/clients"
 
-Template.clientList.onCreated(function(){
-  Meteor.subscribe("Clients");
-});
+Template.clientList.onCreated(function () {
+  Meteor.subscribe("Clients")
+})
 
 Template.clientList.helpers({
-  client: function(){
-    return ClientsCollection.find({}, {sort: { createdAt: -1}});
+  client: function () {
+    return ClientsCollection.find({}, { sort: { createdAt: -1 } })
   },
-  provisionedClients: function(){
-    return ClientsCollection.find({provisioned: true}, {sort: { createdAt: -1}});
-  }
-});
+  provisionedClients: function () {
+    return ClientsCollection.find(
+      { provisioned: true },
+      { sort: { createdAt: -1 } }
+    )
+  },
+})
 
 Template.clientList.events({
-  "click .provision-button": function(event) {
-    event.preventDefault();
+  "click .provision-button": function (event) {
+    event.preventDefault()
 
-    console.log("Hostname:", this.hostname);
+    console.log("Hostname:", this.hostname)
 
-    Meteor.call('Clients.Provision', this._id, function(err, res){
-      if(err){
-        return console.error(err);
+    Meteor.call("Clients.Provision", this._id, function (err, res) {
+      if (err) {
+        return console.error(err)
       }
 
-      console.log('Success!', res);
-    });
-  }
-});
+      console.log("Success!", res)
+    })
+  },
+})
