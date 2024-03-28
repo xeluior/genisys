3902ec45d130d7b5b66b5fbe6b37a7892b957bfc
Henry Green <greeht01@pfw.edu>
2024-03-28 16:31:04 -0400

    Added ansible command to client.provision
    Also added inventory file creation to startup command instead of method call
    
    Also changed structure of inventory file to better reflect what we're actually trying to do.

--- meteor-dev/api/clients/server/methods.js
+++ meteor-dev/api/clients/server/methods.js
@@ -3,9 +3,11 @@ import { check } from "meteor/check"
 import { ClientsCollection } from "../clients"
 import fs from "fs"
 const { exec } = require('child_process')
+import { Mongo } from 'meteor/mongo';
+
 Meteor.methods({
   "Clients.Provision": function (clientId, playbook) {
-    check(clientId, String)
+    check(clientId, Mongo.ObjectID)
 
     const client = ClientsCollection.findOne({
       _id: clientId,
@@ -15,14 +17,7 @@ Meteor.methods({
       throw new Meteor.Error("client-not-found", "That client doesn't exist.")
     }
 
-    fs.access("inventory", fs.constants.F_OK, (err) => {
-      if (err) {
-        console.log(`inventory does not exist, creating now`)
-        fs.writeFileSync("inventory", "")
-      } else {
-        console.log(`inventory exists`)
-      }
-    })
+
 
     // Reading inventory file and adding hostname to inventory file
     fs.readFile('inventory', 'utf8', function (err, fileContent) {
@@ -31,12 +26,12 @@ Meteor.methods({
             return;
         }
     
-        // Check if the hostname
+        // Check if the hostname is in the file 
         if (fileContent.includes(client.hostname)) {
             console.log(`${client.hostname} already exists in the file.`);
         } else {
-            // If 'client' is not in the file, add it to the end of the file
-            fileContent += `${client.hostname}\n`;
+            // If 'client.hostname' is not in the file, add it to the end of the file
+            fileContent += `${client.hostname} ansible_host=${client.ip}\n`;
     
             // Write the modified content back to the file
             fs.writeFile('inventory', fileContent, 'utf8', function (err) {
@@ -51,7 +46,7 @@ Meteor.methods({
     });
 
     // Running Ansible command
-    const command = `ansible-playbook -i inventory ${playbook} --limit ${client.hostname}`
+    const command = `ansible-playbook -i inventory ${playbook} --limit "${client.hostname}"`
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error.message}`);
--- meteor-dev/server/main.js
+++ meteor-dev/server/main.js
@@ -7,20 +7,20 @@ import "../api/clients/server/methods"
 const yaml = require("js-yaml")
 const fs = require("fs")
 
-// const CONFIG_FILE_VAR = process.env.CONFIG_FILE || "/etc/genisys.yaml"
-// export const CONFIG_FILE = yaml.load(
-//   fs.readFileSync(String(CONFIG_FILE_VAR), "utf8")
-// )
+const CONFIG_FILE_VAR = process.env.CONFIG_FILE || "/etc/genisys.yaml"
+export const CONFIG_FILE = yaml.load(
+  fs.readFileSync(String(CONFIG_FILE_VAR), "utf8")
+)
 
-const temp_yaml_string = `---
-ansible:
-  inventory: /var/genisys/inventory
-  ssh-key: /etc/genisys/ssh/id_rsa
-  playbooks:
-    - /etc/genisys/playbooks/firstrun.yaml
-    - /etc/genisys/playbooks/script2.yaml`
+// const temp_yaml_string = `---
+// ansible:
+//   inventory: /var/genisys/inventory
+//   ssh-key: /etc/genisys/ssh/id_rsa
+//   playbooks:
+//     - /etc/genisys/playbooks/firstrun.yaml
+//     - /etc/genisys/playbooks/script2.yaml`
 
-export const CONFIG_FILE = yaml.load(temp_yaml_string)
+// export const CONFIG_FILE = yaml.load(temp_yaml_string)
 
 Meteor.startup(() => {
   console.log("Meteor Started")
@@ -29,9 +29,16 @@ Meteor.startup(() => {
 
   CONFIG_FILE["ansible"]["playbooks"].forEach((element) => {
     obj = { playbook: element }
-    // console.log(obj)
     PlaybooksCollection.insert(obj)
   })
 
-  //   ClientsCollection.insert({ hostname: "genisys_Test", ip: "10.0.0.1" })
+  //Creating Inventory file
+  fs.access("inventory", fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log(`inventory does not exist, creating now`)
+      fs.writeFileSync("inventory", "[all_hosts]\n")
+    } else {
+      console.log(`inventory exists`)
+    }
+  })
 })
