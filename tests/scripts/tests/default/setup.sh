00643c3b8591d1730592eb0fd751acf1efb3c876
Robert Gingras <developer@three-point-five.dev>
2024-04-03 14:28:10 -0400

    add useradd to script to prevent error

--- tests/scripts/tests/default/setup.sh
+++ tests/scripts/tests/default/setup.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+sudo useradd -d /srv/genisys -m -r -s /sbin/nologin -U genisys
 sudo pip install /app/genisys-0.1.0-py3-none-any.whl
 sudo genisys install --file /app/data/config.yaml
