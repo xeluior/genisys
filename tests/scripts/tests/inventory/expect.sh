9d8b04e579dba4bcd6a75eb5de785066623dc689
Robert Gingras <developer@three-point-five.dev>
2024-04-12 09:37:34 -0400

    preserve environment when launching genisys server

--- tests/scripts/tests/inventory/expect.sh
+++ tests/scripts/tests/inventory/expect.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-CONFIG_FILE=/app/data/config.yaml
-MONGO_URL=mongodb://localhost:3000/local
+export CONFIG_FILE=/app/data/config.yaml
+export MONGO_URL=mongodb://localhost:3000/local
 
-sudo genisys server -f $CONFIG_FILE &
+sudo -E genisys server -f $CONFIG_FILE &
 mongosh $MONGO_URL --eval 'while (db["ClientsCollection"].findOne() == null); console.log("Client Added")'
 sudo killall genisys
 exit 0
