dc2f5dc1b277663a4be67d06578d1f43bbbec2e9
Henry Green <greeht01@pfw.edu>
2024-04-16 13:23:39 -0400

    fail test 2

--- genisys/server/__init__.py
+++ genisys/server/__init__.py
@@ -135,7 +135,7 @@ def meteor_initialization(server_config: ServerOptions):
         old_cwd = os.getcwd()
         meteor_dev_dir = Path(package_location[:package_location.rfind('/')], 'meteor-dev')
         os.chdir(meteor_dev_dir)
-        subprocess.run(['meteor', 'test', '--driver-package', 'meteortesting:mocha', '--once', '--full-app'], check=True)
+        subprocess.run(['meteor', 'test', '--driver-package', 'meteortesting:mocha', '--once', '--full-app'], check=True, stderr=subprocess.STDOUT)
         os.chdir(old_cwd)
         return
 
